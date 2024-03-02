fa37b0870dc76dda7c583580327c1051ba6664be
aami-12 <iamaamir253@gmail.com>
2024-03-02 21:34:17 +0530

    changes

--- Backend/dist/Backend/src/config/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://hotelbooking-doie.onrender.com'
-];
-exports.default = allowedOrigins;
