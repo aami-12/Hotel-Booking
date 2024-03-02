5dd8cd257f097f3a42518c68b789288e80266973
aami-12 <iamaamir253@gmail.com>
2024-03-02 23:12:02 +0530

    chsanges

--- Frontend/src/api-client.ts
+++ Frontend/src/api-client.ts
@@ -2,7 +2,7 @@ import { SignInFormData } from "./Pages/Signin";
 import { RegisterFormData } from "./Pages/Register";
 import { HotelSearchResponse, HotelType, PaymentIntentResponse, UserType } from "../../Backend/src/sharedTypes/types";
 import { BookingFormData } from "./forms/BookingForm/BookingForm";
-const API_BASE_URL = "http://hotelbooking-apis.onrender.com" || "";
+const API_BASE_URL = "https://hotelbooking-apis.onrender.com" || "";
 
 
 export const fetchCurrentUser = async () : Promise<UserType> => {
